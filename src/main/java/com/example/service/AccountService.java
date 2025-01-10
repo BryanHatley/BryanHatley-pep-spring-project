a856bbd6fd3189a0406a2e0a922149a130f1e24f
BryanHatley <bryan.hatley2019@gmail.com>
2025-01-10 03:38:48 -0500

    Account Service, Repository ready

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,43 @@
 package com.example.service;
 
-public class AccountService {
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+
+
+@Service
+public class AccountService 
+{
+    AccountRepository accountRepository;
+
+    @Autowired
+    public AccountService(AccountRepository accountRepository)
+    {
+        this.accountRepository = accountRepository;
+    }
+
+    public Account persistAccount(Account account)
+    {
+        return accountRepository.save(account);
+    }
+
+    public Account getAccountByUsername(Account account)
+    {
+        Optional<Account> optionalAccount = accountRepository.findAccountByUsernameAndPassword(
+                                            account.getUsername(), account.getPassword());
+        
+        if(optionalAccount.isPresent())
+        {
+            return optionalAccount.get();
+        }
+        else
+        {
+            return null;
+        }
+            
+    }
 }
